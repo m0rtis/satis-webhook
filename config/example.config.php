0cb5a11687af51bb9bec121f122fcb362266a795
m0rtis <mail@m0rtis.ru>
2018-04-14 15:51:46 +0300

    Changed default and example config

--- config/example.config.php
+++ config/example.config.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 return [
     'secret' => 'someRandomLongString',
     'uri_key' => 'SomeSecretKeyAsPartOfURI',
-    'satis_config' => __DIR__.'/satis.json',
-    'output_dir' => __DIR__.'/../public/'
+    'satis_config' => __DIR__ .'/satis.json',
+    'output_dir' => __DIR__.'/../public'
 ];
