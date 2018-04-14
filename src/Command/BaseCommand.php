0249a7ce49b72483b824a7a3bc7c31d1c58e9a3a
m0rtis <mail@m0rtis.ru>
2018-04-14 15:50:59 +0300

    AddCommand is unnessesary now. UpdateRepositoryCommand can add a package in repository automatically.

--- src/Command/BaseCommand.php
+++ src/Command/BaseCommand.php
@@ -5,13 +5,17 @@ declare(strict_types=1);
 namespace Composer\Satis\Webhook\Command;
 
 
+use Composer\IO\NullIO;
 use Composer\Satis\Console\Application;
 use Composer\Satis\Webhook\Config\CommandValidator;
 use Composer\Satis\Webhook\Provider\ProviderInterface;
+use Composer\Satis\Webhook\UnauthorizedRequestException;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\CommandNotFoundException;
 use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Output\NullOutput;
+use Symfony\Component\Console\Output\BufferedOutput;
 
 /**
  * Class BaseCommand
@@ -28,7 +32,7 @@ abstract class BaseCommand
      */
     protected $input;
     /**
-     * @var NullOutput
+     * @var BufferedOutput
      */
     protected $output;
 
@@ -42,16 +46,42 @@ abstract class BaseCommand
     }
 
     /**
-     * @return Application
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param array $args
+     * @return ResponseInterface
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
+     * @throws \Exception
      */
-    protected function setUp(): Application
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
-        $this->input = new ArrayInput([
-            'file' => $this->config->get('satis_config'),
-            'output-dir' => $this->config->get('output_dir')
-        ]);
-        $this->output = new NullOutput();
-        return new Application();
+        $answer = '';
+        try {
+            $answer = $this->execute($request, $args['provider']);
+        } catch (UnauthorizedRequestException $e) {
+            $response = $response->withStatus(401);
+            //TODO: To implement logging
+        } catch (\Exception $e) {
+            $response = $response->withStatus(500);
+            $answer = $e->getMessage();
+        }
+        $response->getBody()->write($answer);
+        return $response;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param string $provider
+     * @return string
+     * @throws \InvalidArgumentException
+     * @throws UnauthorizedRequestException
+     * @throws \ReflectionException
+     */
+    protected function execute(ServerRequestInterface $request, string $provider): string
+    {
+        $request = $this->prepareRequest($request, $provider);
+        return $this->runSatisCommands($request);
     }
 
     /**
@@ -72,10 +102,65 @@ abstract class BaseCommand
     }
 
     /**
-     * @param ServerRequestInterface $request
-     * @param ResponseInterface $response
-     * @param array $args
-     * @return ResponseInterface
+     * @param string $commandName
+     * @return Command
+     * @throws CommandNotFoundException
+     * @throws \ReflectionException
      */
-    abstract public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args);
+    protected function getCommand(string $commandName): Command
+    {
+        $app = new Application();
+        $command = $app->find($commandName);
+        $command = $this->configureCommand($command, $app);
+        return $command;
+    }
+
+    /**
+     * @param Command $command
+     * @param Application $app
+     * @return Command
+     * @throws \ReflectionException
+     */
+    private function configureCommand(Command $command, Application $app): Command
+    {
+        $io = new NullIO();
+        $refApp = new \ReflectionClass($app);
+        $ioProp = $refApp->getProperty('io');
+        $ioProp->setAccessible(true);
+        $ioProp->setValue($app, $io);
+        $command->setApplication($app);
+        return $command;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param string $provider
+     * @return ServerRequestInterface
+     * @throws UnauthorizedRequestException
+     * @throws \InvalidArgumentException
+     * @throws \ReflectionException
+     */
+    private function prepareRequest(ServerRequestInterface $request, string $provider): ServerRequestInterface
+    {
+            $provider = $this->getProvider($provider);
+            $request = $provider->provide($request);
+            if (isset($this->config['secret'])) {
+                $this->checkSecret($request);
+            }
+            return $request;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @throws UnauthorizedRequestException
+     */
+    private function checkSecret(ServerRequestInterface $request): void
+    {
+        $givenSecret = $request->getAttribute('secret');
+        if ($givenSecret !== $this->config['secret']) {
+            throw new UnauthorizedRequestException($givenSecret);
+        }
+    }
+
+    abstract protected function runSatisCommands(ServerRequestInterface $request): string;
 }
--- src/Command/UpdateRepositoryCommand.php
+++ src/Command/UpdateRepositoryCommand.php
@@ -5,35 +5,70 @@ declare(strict_types=1);
 namespace Composer\Satis\Webhook\Command;
 
 
-use Psr\Http\Message\ResponseInterface;
+use Composer\Json\JsonFile;
+use Composer\Satis\Console\Command\AddCommand;
+use Composer\Satis\Console\Command\BuildCommand;
 use Psr\Http\Message\ServerRequestInterface;
+use Symfony\Component\Console\Exception\CommandNotFoundException;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 
-final class UpdateRepositoryCommand extends BaseCommand
+class UpdateRepositoryCommand extends BaseCommand
 {
-
     /**
      * @param ServerRequestInterface $request
-     * @param ResponseInterface $response
-     * @param array $args
-     * @return ResponseInterface
+     * @return string
      * @throws \RuntimeException
+     * @throws CommandNotFoundException
      * @throws \InvalidArgumentException
      * @throws \ReflectionException
+     * @throws \Exception
      */
-    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args)
+    protected function runSatisCommands(ServerRequestInterface $request): string
     {
-        $provider = $this->getProvider($args['provider']);
-        $request = $provider->provide($request);
-        $app = $this->setUp();
-        $this->input->setArgument('packages', $request->getAttribute('package_name'));
-        $this->input->setArgument('command', 'build');
-        try {
-            $app->run($this->input, $this->output);
-        } catch (\Exception $e) {
-            $body = $response->getBody();
-            $body->write($e->getMessage());
-            $response = $response->withBody($body);
+        $output = new BufferedOutput();
+        $packageName = $request->getAttribute('package_name');
+        $packageUrl = $request->getAttribute('package_url');
+        $satisJson = $this->config['satis_config'];
+
+        if ($this->packageDoesNotExists($packageUrl)) {
+            $inputAdd = new ArrayInput([
+                'file' => $satisJson,
+                'url' => $packageUrl
+            ]);
+            /** @var AddCommand $add */
+            $add = $this->getCommand('add');
+            $add->run($inputAdd, $output);
         }
-        return $response;
+        $inputBuild = new ArrayInput([
+            'file' => $satisJson,
+            'output-dir' => $this->config['output_dir'],
+            'packages' => [$packageName]
+        ]);
+        /** @var BuildCommand $build */
+        $build = $this->getCommand('build');
+        $build->run($inputBuild, $output);
+
+        return $output->fetch();
+    }
+
+    /**
+     * @param string $packageUrl
+     * @return bool
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
+     */
+    private function packageDoesNotExists(string $packageUrl): bool
+    {
+        $file = new JsonFile($this->config['satis_config']);
+        $config = $file->read();
+        if (isset($config['repositories']) && \is_array($config['repositories'])) {
+            foreach ($config['repositories'] as $repository) {
+                if ($repository['url'] === $packageUrl) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
