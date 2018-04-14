c41648c1572312f35c96ca415680f70c8fda1595
m0rtis <mail@m0rtis.ru>
2018-04-14 15:52:08 +0300

    Changed default and example config

--- src/Config/default.config.php
+++ src/Config/default.config.php
@@ -1,39 +1,28 @@
 <?php
 declare(strict_types=1);
 
-use Composer\Satis\Webhook\AuthMiddleware;
 use Composer\Satis\Webhook\Command\AddPackageCommand;
 use Composer\Satis\Webhook\Command\UpdateRepositoryCommand;
-use Composer\Satis\Webhook\Container\ContainerFactory;
+use m0rtis\SimpleBox\ContainerFactory;
 use Psr\Container\ContainerInterface;
 use Slim\Interfaces\RouterInterface;
 
 return [
     ContainerInterface::class => [
-        'factory_class' => ContainerFactory::class,
-        'definitions' => [
-
-        ]
+        'factory_class' => ContainerFactory::class
     ],
     RouterInterface::class => [
         'root' => [
             'pattern' => '/command',
             'type' => 'group',
             'routes' => [
-                'add' => [
-                    'pattern' => '/{provider}/add/',
-                    'type' => 'route',
-                    'methods' => ['POST'],
-                    'handler' => AddPackageCommand::class,
-                ],
                 'update' => [
                     'pattern' => '/{provider}/update/',
                     'type' => 'route',
                     'methods' => ['POST'],
                     'handler' => UpdateRepositoryCommand::class
                 ]
-            ],
-            'middleware' => AuthMiddleware::class
+            ]
         ]
-    ],
+    ]
 ];
